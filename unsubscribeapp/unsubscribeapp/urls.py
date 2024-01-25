960730473f141740d2edb3b7c8b406ac4af280a2
Dmytro Chumachenko <104909647+chumachenkoo@users.noreply.github.com>
2024-01-25 17:26:11 +0100

    Update urls.py

--- unsubscribeapp/unsubscribeapp/urls.py
+++ unsubscribeapp/unsubscribeapp/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('get-emails/', views.download_emails, name='get_emails'),
-    path('edit-emails/', views.edit_emails, name='edit_emails'),
-    path('delete-emails/', views.delete_emails, name='delete_emails'),
-    path('download-emails/', views.download_emails, name='download_emails'),
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
 ]
