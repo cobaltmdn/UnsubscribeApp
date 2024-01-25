e7033cba1c2a21c867c4e3bf9d51acd35e369903
Dmytro Chumachenko <104909647+chumachenkoo@users.noreply.github.com>
2024-01-25 17:42:49 +0100

    Update urls.py

--- unsubscribeapp/myapp/urls.py
+++ unsubscribeapp/myapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get-emails/', views.download_emails, name='get_emails'),
+    path('get-emails/', views.get_emails, name='get_emails'),
     path('edit-emails/', views.edit_emails, name='edit_emails'),
     path('delete-emails/', views.delete_emails, name='delete_emails'),
     path('download-emails/', views.download_emails, name='download_emails'),
