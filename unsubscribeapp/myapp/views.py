81ead2e059167719a3553fee1de0359a4173812d
Dmytro Chumachenko <104909647+chumachenkoo@users.noreply.github.com>
2024-01-25 17:42:17 +0100

    Update views.py

--- unsubscribeapp/myapp/views.py
+++ unsubscribeapp/myapp/views.py
@@ -21,20 +21,6 @@ def index(request):
         return render(request, 'index.html')
 
 
-# def download_emails(request):
-#     if request.method == 'POST':
-#         password = request.POST.get('password')
-#         if password == 'admin':
-#             emails = Email.objects.all()
-#             email_list = "\n".join([str(email) for email in emails])
-#             response = HttpResponse(email_list, content_type='text/plain')
-#             response['Content-Disposition'] = 'attachment; filename="emails.txt"'
-#             return response
-#         else:
-#             return render(request, 'download.html', {'error_message': 'Invalid password'})
-#
-#     return render(request, 'download.html')
-
 def get_emails(request):
     if request.method == 'POST':
         password = request.POST.get('password')
@@ -47,10 +33,8 @@ def get_emails(request):
 
 
 def edit_emails(request):
-    # emails = Email.objects.all()
-    # return render(request, 'edit_emails.html', {'emails': emails})
     email_list = Email.objects.all().order_by('id')
-    paginator = Paginator(email_list, 200)  # Show 200 emails per page
+    paginator = Paginator(email_list, 200)
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
