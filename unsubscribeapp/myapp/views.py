5c05813101062cf32c59fc6cc25f7046e31403e3
Dmytro Chumachenko <104909647+chumachenkoo@users.noreply.github.com>
2024-01-29 15:32:38 +0100

    Update views.py

--- unsubscribeapp/myapp/views.py
+++ unsubscribeapp/myapp/views.py
@@ -33,13 +33,16 @@ def get_emails(request):
 
 
 def edit_emails(request):
-    email_list = Email.objects.all().order_by('id')
-    paginator = Paginator(email_list, 200)
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    return render(request, 'edit_emails.html', {'page_obj': page_obj})
+    if request.method == 'POST':
+        email_list = Email.objects.all().order_by('id')
+        paginator = Paginator(email_list, 200)
+    
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+    
+        return render(request, 'edit_emails.html', {'page_obj': page_obj})
+    else:
+        return render(request, 'download.html')
 
 
 def delete_emails(request):
